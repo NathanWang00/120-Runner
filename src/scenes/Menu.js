1961aa39204be1e2083667c8251a18ebaa412362
Nathan Wang <nxwang@ucsc.edu>
2021-04-23 21:18:23 -0700

    Background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,7 +2,7 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
-
+    
     create() {
         this.scene.start("playScene");
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,7 +2,21 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
+    preload() {
+        this.load.image('road', './assets/road.png');
+    }
+
     create() {
-        console.log("test");
+        // place road sprite
+        this.starfield = this.add.tileSprite(0, 350, 1280, 436, 'road').setOrigin(0, 0);
+
+        // add camera
+        this.camera = this.cameras.add();
+
+        this.camera.setBackgroundColor('rgba(255, 255, 255, 1)');
+    }
+
+    update() {
+        this.starfield.tilePositionX += 4;
     }
 }
