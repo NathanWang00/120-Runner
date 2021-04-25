c412298a328ed119a7060250b9da4a3782be9943
Nathan Wang <nxwang@ucsc.edu>
2021-04-24 17:35:18 -0700

    player animation!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,8 +2,10 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
+
     preload() {
         this.load.image('road', './assets/road.png');
+        this.load.atlas('pTexture', './assets/pTexture.png', './assets/pTexture.json');
     }
 
     create() {
@@ -12,8 +14,25 @@ class Play extends Phaser.Scene {
 
         // add camera
         this.camera = this.cameras.add();
-
         this.camera.setBackgroundColor('rgba(255, 255, 255, 1)');
+
+        // create player
+
+        this.anims.create({ 
+            key: 'test', 
+            frames: this.anims.generateFrameNames('pTexture', {
+                start: 1,
+                end: 8,
+                zeroPad: 1,
+                prefix: 'run',
+                suffix: '.gif'
+            }),
+            frameRate: 12, 
+            repeat: -1
+        });
+        this.player = this.add.sprite(200, 400, 'run');
+        this.player.setScale(0.5, 0.5);
+        this.player.anims.play('test');
     }
 
     update() {
