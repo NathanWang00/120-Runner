dad0cfdb7a1d30b1f7932c006e8a68c6edb451ba
Nathan Wang <nxwang@ucsc.edu>
2021-04-24 22:09:32 -0700

    Player Jump

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -34,17 +34,54 @@ class Play extends Phaser.Scene {
             frameRate: 12, 
             repeat: -1
         });
+
+        this.anims.create({ 
+            key: 'jumpAnim', 
+            frames: this.anims.generateFrameNames('pTexture', {
+                start: 2,
+                end: 15,
+                zeroPad: 2,
+                prefix: 'jump',
+                suffix: '.gif'
+            }),
+            frameRate: 12, 
+            repeat: 0
+        });
         this.player = this.physics.add.sprite(200, 350, 'run');
-        this.player.setScale(0.32, 0.32);
-        this.player.setSize(375, 350, false);
-        this.player.setOffset(60, 0);
-        this.player.anims.play('runAnim');
+        this.PlayerRun();
 
         this.physics.add.collider(this.player, this.road);
         this.player.setCollideWorldBounds(true);
+
+        // controls
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
         this.road.tilePositionX += 6;
+        if(this.player.body.touching.down && this.player.anims.currentAnim.key === 'jumpAnim') {
+            this.PlayerRun();
+        }
+
+        if(this.cursors.up.isDown && this.player.body.touching.down) {
+            this.PlayerJump();
+        }
+        
+    }
+
+    PlayerRun(){
+        this.player.setScale(0.32, 0.32);
+        this.player.setSize(375, 350, false);
+        this.player.setOffset(60, 0);
+        this.player.anims.play('runAnim');
+    }
+
+    PlayerJump(){
+        this.player.setVelocityY(-500);
+        this.player.anims.play('jumpAnim');
+        this.player.setSize(325, 500, false);
+        this.player.setOffset(60, 0);
+        this.player.setScale(0.25, 0.25);
+        
     }
 }
